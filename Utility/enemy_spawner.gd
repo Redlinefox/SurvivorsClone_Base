2c042dd84e3df929f496ea27335f9b4dd2f8aa49
Aric Akbani <aricakbani@gmail.com>
2024-11-24 01:15:45 -0600

    Updated enemy spawner
    Connected the enemy spawner signal.  Added a get_random_radius_position function to spawn enemies in a circle around the Player.

--- Utility/enemy_spawner.gd
+++ Utility/enemy_spawner.gd
@@ -13,9 +13,9 @@ func _on_timer_timeout() -> void:
 	
 	for i in spawns:
 		# If the time counter is between "start" and "end" 
-		print("spawning enemies")
 		if time >= i.time_start and time <= i.time_end:
 			# check if the spawn delay counter is less than the spawn delay time, if yes advance spawn delay counter by 1.
+			print("Time:", time)
 			if i.spawn_delay_counter < i.enemy_spawn_delay:
 				i.spawn_delay_counter += 1
 			else:
@@ -27,6 +27,7 @@ func _on_timer_timeout() -> void:
 				while counter < i.enemy_number:
 					var enemy_spawn = new_enemy.instantiate()
 					enemy_spawn.global_position = get_random_viewport_position()
+					#enemy_spawn.global_position = get_random_radius_position()
 					add_child(enemy_spawn)
 					counter += 1
 
@@ -38,7 +39,7 @@ func get_random_viewport_position():
 	var top_right = Vector2(player.get_global_position().x + vpr.x/2, player.get_global_position().y - vpr.y/2)
 	var bottom_left = Vector2(player.get_global_position().x - vpr.x/2, player.get_global_position().y + vpr.y/2)
 	var bottom_right = Vector2(player.get_global_position().x + vpr.x/2, player.get_global_position().y + vpr.y/2)
-	var position_side = ["up", "down", "left", "right"]
+	var position_side = ["up", "down", "left", "right"].pick_random()
 	var spawn_position1 = Vector2.ZERO
 	var spawn_position2 = Vector2.ZERO
 	
@@ -58,9 +59,21 @@ func get_random_viewport_position():
 	
 	var x_spawn = randf_range(spawn_position1.x, spawn_position2.x)
 	var y_spawn = randf_range(spawn_position1.y, spawn_position2.y)
-	
 	return Vector2(x_spawn, y_spawn)
 
 
 func get_random_radius_position():
-	pass
+	# set radius distance
+	var spawn_radius_min = 100
+	var spawn_radius_max = 200
+	# get random angle from he circle constant, the circumference of the unit circle in radians
+	var angle = randf() * TAU
+	# set a random distance between the minimum and maximum spawn radius
+	var distance = randf_range(spawn_radius_min, spawn_radius_max)
+	# create the x and y offset distance from the player
+	var offset_x = cos(angle) * distance
+	var offset_y = sin(angle) * distance
+	print(offset_x," ", offset_y)
+	return player.global_position + Vector2(offset_x, offset_y)
+	
+	
